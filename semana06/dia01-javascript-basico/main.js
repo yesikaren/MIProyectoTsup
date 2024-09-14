df88346637748df113f821c6dbe50ef33d05b7db
yesikaren <chuichalcoyesica@gmail.com>
2024-09-13 19:16:58 -0500

    js intermedio

--- semana06/dia01-javascript-basico/main.js
+++ semana06/dia01-javascript-basico/main.js
@@ -1,1 +1,136 @@
-console.log("hola desde el main")
+console.log(20)
+console.log(15.69)
+console.log(0.9999)
+console.log(-36)
+
+console.log("claudia")
+console.log('Yesica')
+console.log('200')
+
+console.log(typeof 20)
+console.log(typeof 15.69)
+console.log(typeof 0.9999)
+console.log(typeof -36)
+console.log(typeof "claudia")
+console.log(typeof 'Yesica')
+console.log(typeof '200')
+
+console.log(true)
+let nombre 
+console.log(nombre)
+console.log(typeof nombre)
+let apellido = null 
+console.log(apellido)
+console.log(typeof apellido)
+
+const edad = 55
+
+const _edad = 25
+const PI = 3.14 
+console.log(edad)
+const esMayouDeEdad = false
+
+let edad2 = 35 
+
+edad2 = 39
+console.log(typeof edad2)
+edad2 = "Yesica"
+console.log(typeof edad2)
+
+console.log('1121'+ 15 + 19)
+
+// Operadores matematicos 
+console.log(1+2)
+console.log(3-2)
+console.log(2*2)
+console.log(1/2)
+console.log(10%4)
+console.log(2**3)
+console.log(Math.pow(2,3))
+
+const genero = 'femenino'
+console.log(genero)
+
+console.log(1==1)
+console.log(1==='1')
+console.log(1 != '1')
+
+console.log(1===1)
+console.log(1==='1')
+console.log(1 !== '1')
+
+console.log(8>5)
+console.log(5>1)
+console.log(8>=5)
+console.log(8<=5)
+
+
+console.log(true && false)
+console.log(true || false)
+console.log(!true)
+
+
+const saludo = 'Hola,'
+const nombreCompleto = 'Yesica' + '' + 'Chui'
+const imprimirDaludo = saludo + nombreCompleto + ".Tengo"+ edad2 + 'años'
+
+console.log(imprimirDaludo.length)
+console.log(nombreCompleto)
+const cadena1='karen'
+const cadena2='chuki'
+
+console.log(cadena1.length===cadena2.length)
+
+let numero = 22 
+console.log(numero < 40 )
+console.log(numero < 60 )
+console.log(numero % 2 == 0 )
+console.log(numero % 2 !== 0 )
+
+let base = 2
+let altura = 4
+console.log(base*altura/2 )
+
+const numeroTresCifras = 452
+console.log(numeroTresCifras**3)
+
+let dinero = 1000
+
+//
+//
+if(true){
+
+}
+const numerox = 33
+const esPar = numero % 2 === 0 
+
+if(esPar){
+  console.log('Este numero es par', numerox)
+}
+
+if(true){
+
+}else{
+
+}
+
+if(esPar){
+  console.log('Este numero es par', numerox)
+  
+}else{
+  console.log('Este numero es impar', numerox)
+}
+
+if (true){
+  console.log('Este numero es impar', numerox)
+  
+}else if (false){
+  console.log('Este numero es impar', numerox)
+  
+}else if (true){
+  console.log('Este numero es impar', numerox)
+
+}else{
+
+}
+
---
+++ semana06/dia02-javascript-intermedio/main.js
@@ -0,0 +1,182 @@
+console.log("Hola JS");
+
+const arregloVacio = [];
+const listaDeNombres = [
+  "Yesica",
+  "Karen",
+  "Efrain",
+  "Will",
+  "Carlos",
+  "Federico",
+];
+const listaDeNumeros = [23, 34, 18, 190];
+const listaDeValores = [1, 4, 5, "Alex", true, null, undefined];
+console.log(arregloVacio);
+console.log(listaDeNombres);
+console.log(listaDeNumeros);
+console.log(listaDeValores);
+console.log(listaDeNombres[2]);
+console.log(listaDeNombres[299]);
+console.log(listaDeNombres[0]);
+console.log(listaDeNombres[listaDeNombres.length - 1]);
+listaDeNombres[0] = "Chui";
+console.log(listaDeNombres);
+
+const listaFrutas = ["manzana", "PERA", "platano", "mandarina", "fresa"];
+listaFrutas.push("papaya");
+console.log(listaFrutas);
+/* const eliminadoFruta = listaFrutas.pop()
+listaFrutas.pop()
+console.log(listaFrutas)
+console.log(eliminadoFruta) */
+
+// listaFrutas.splice(2,2,'mango','sandia')
+console.log(listaFrutas);
+const duplicadoFrutas = listaFrutas.slice();
+
+console.log(listaFrutas.at(-2));
+
+console.log(listaFrutas.includes("pera"));
+insertarFruta("uva");
+insertarFruta("sandia");
+insertarFruta("platano");
+function insertarFruta(fruta) {
+  if (listaFrutas.includes(fruta)) {
+    console.log(`Fruta ${fruta} repetida`);
+    console.log(listaFrutas);
+  } else {
+    listaFrutas.push(fruta);
+    console.log(listaFrutas);
+  }
+}
+console.log("////otro///");
+console.log(listaFrutas.filter((fruta) => fruta.toUpperCase().includes("DA")));
+
+const frutasNuevas = listaFrutas.map((fruta) => {
+  if (fruta.includes("p")) {
+    return "papita";
+  }
+  return fruta.toUpperCase();
+});
+
+console.log(frutasNuevas);
+console.log(listaFrutas);
+
+console.log(frutasNuevas.sort());
+
+const frutasDuplicadas = [...listaFrutas];
+console.log(frutasDuplicadas);
+
+const sueldos = [1200, 1500, 1800, 2100, 2500, 3000, 3500];
+
+console.log(sueldos);
+const sumatoriaSueldos = sueldos.reduce((acu, sueldo) => acu + sueldo);
+console.log(sumatoriaSueldos);
+
+let estudiante = {
+  nombre: "María",
+  apellido:"Lopez",
+  "padre de familia":"Juan Perez",
+  edad: 10,
+  grado: "5to de Primaria",
+  promedio: 9.2,
+  materias: [ "Matemáticas", "Ciencias", "Lenguaje", "Historia", "Educación Física", ],
+  activo: true,
+  asistencia: 95, // porcentaje de asistencia
+  obtenerNombreCompleto: function () {
+    return this.nombre+" "+this.apellido;
+  },
+  pasaDeGrado: function () {
+    return this.promedio >= 6.0; // Consideramos que pasa con un promedio mayor o igual a 6.0
+  },
+  asistenciaSuficiente: function () {
+    return this.asistencia >= 85; // Necesita un 85% o más de asistencia
+  },
+};
+console.log(estudiante.obtenerNombreCompleto())
+console.log('Mis materias son: '+estudiante.materias.join(', '))
+
+console.log(estudiante["padre de familia"])
+let miClave='nombre'
+console.log(estudiante[miClave])
+
+leerDato('apellido')
+
+function leerDato(miSuperClave){
+  console.log(estudiante[miSuperClave])
+}
+
+console.log(estudiante.materias[2])
+
+const {nombre:nombre2,edad,apellido}=estudiante
+console.log(nombre2)
+console.log(apellido)
+console.log(edad)
+
+const misDatos=[3,10]
+const [dato1,dato2]=misDatos
+
+console.log(dato1+dato2)
+
+
+
+
+function useState(datoInicial) {
+  // Creamos un objeto para que se mantenga la referencia
+  let estado = datoInicial;
+  
+  // Función para modificar el valor
+  function setValor(nuevoValor) {
+    estado = nuevoValor;
+    console.log("Nuevo valor:", estado);
+  }
+  
+  // Retornamos el valor actual y la función para modificarlo
+  return [estado, setValor];
+}
+
+const [midato,setMidato] =useState(4)
+
+console.log(midato)
+
+setMidato(10)
+console.log(midato)
+
+
+
+
+console.log(Object.keys(estudiante))
+console.log(Object.values(estudiante))
+console.log(Object.entries(estudiante))
+
+const super_hero ={
+  'Batman':'Hola soy bruce',
+  'Spiderman':'Hola soy Peter',
+  'IronMan':'Hola soy Tony'
+}
+const Heroe='IronMannnn'
+console.log(super_hero[Heroe]??'No eres un Heroe')
+
+/*
+Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+*/
+
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+function contarOvejas(ovejas) {
+  // aquí tu magia
+  const ovejasFiltradas = ovejas.filter((oveja)=>(oveja.color=='rojo')&&(oveja.name.toUpperCase().includes('N'))&&(oveja.name.toUpperCase().includes('A')))
+  
+  return ovejasFiltradas
+}
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
