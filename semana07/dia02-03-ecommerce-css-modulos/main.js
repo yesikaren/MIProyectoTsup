ab9d45e3128245d966235df487804a09fabfa158
yesikaren <chuichalcoyesica@gmail.com>
2024-09-25 18:06:59 -0500

    ejercicio cantidad ecommerce

--- semana07/dia02-03-ecommerce-css-modulos/main.js
+++ semana07/dia02-03-ecommerce-css-modulos/main.js
@@ -1,1 +1,14 @@
 // TODO: Al hacer click en los botones - y + decremente o incremente la cantidad.
+const aumentar = document.querySelector(".aumentar");
+const disminuir = document.querySelector(".disminuir");
+const cantidad = document.querySelector(".cantidad");
+
+aumentar.addEventListener("click", () => {
+  cantidad.innerHTML = Number(cantidad.innerHTML) + 1;
+  //alert(cantidad.innerHTML)
+});
+disminuir.addEventListener("click", () => {
+  if (Number(cantidad.innerHTML) > 0){
+    cantidad.innerHTML = Number(cantidad.innerHTML) - 1;
+  }
+});
